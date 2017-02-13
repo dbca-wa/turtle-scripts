54c7e2eb8d51e34c7a8c4b50d2b98f6688812ad6
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-02-13 14:00:01 +0800

    update ningaloo db etl

--- upload_workbooks.R
+++ upload_workbooks.R
@@ -1,4 +1,4 @@
 source("config/setup.R")
-ckanr::resource_update(ETL_RID, "ningaloo-etl.html")
-ckanr::resource_update(SPM_RID, "spatial_modelling.html")
-ckanr::resource_update(W2_ETL_RID, "tagging-etl.html")
+ckanr::resource_update(ETL_RID, "ningaloo/ningaloo_etl.html")
+ckanr::resource_update(SPM_RID, "ningaloo/spatial_modelling.html")
+ckanr::resource_update(W2_ETL_RID, "wamtram/wamtram_etl.html")
