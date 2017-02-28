78e41ad02cd70c8e41765581d95dcb8b977624c0
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-03-01 07:58:54 +0800

    add AEM and PCNM source packages, update install script

--- config/install.R
+++ config/install.R
@@ -1,5 +1,16 @@
-install.packages("packfor", repos="http://R-Forge.R-project.org")
+setwd("~/projects/turtle-scripts/")
 install.packages("spacemakeR", repos="http://R-Forge.R-project.org")
 install.packages("adespatial", repos="http://R-Forge.R-project.org")
-install.packages("PCNM", repos="http://R-Forge.R-project.org")
-install.packages("AEM", repos="http://R-Forge.R-project.org")
+install.packages("ape")
+install.packages("spdep")
+install.packages("ade4")
+install.packages("vegan")
+# install.packages("AEM", repos="http://R-Forge.R-project.org")
+install.packages("data/AEM_0.6.tar.gz", repos = NULL, type = "source")
+# install.packages("PCNM", repos="http://R-Forge.R-project.org")
+install.packages("data/PCNM_2.1-4.tgz", repos = NULL, type = .Platform$pkgType)
+
+# require(devtools); install_github("sdray/adespatial") # forward.sel, replaces packfor
+# install.packages("packfor", repos="http://R-Forge.R-project.org")
+install.packages("adespatial")
+
