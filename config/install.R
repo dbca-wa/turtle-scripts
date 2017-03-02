887d507083248c457ce03cc2e2ef9dbb73c208a7
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-03-03 07:35:00 +0800

    clean up install

--- config/install.R
+++ config/install.R
@@ -1,6 +1,5 @@
 setwd("~/projects/turtle-scripts/")
 install.packages("spacemakeR", repos="http://R-Forge.R-project.org")
-install.packages("adespatial", repos="http://R-Forge.R-project.org")
 install.packages("ape")
 install.packages("spdep")
 install.packages("ade4")
@@ -10,7 +9,34 @@ install.packages("data/AEM_0.6.tar.gz", repos = NULL, type = "source")
 # install.packages("PCNM", repos="http://R-Forge.R-project.org")
 install.packages("data/PCNM_2.1-4.tgz", repos = NULL, type = .Platform$pkgType)
 
-# require(devtools); install_github("sdray/adespatial") # forward.sel, replaces packfor
 # install.packages("packfor", repos="http://R-Forge.R-project.org")
-install.packages("adespatial")
+# require(devtools); install_github("sdray/adespatial") # replaces packfor::forward.sel
+install.packages("adespatial", repos="http://R-Forge.R-project.org")
 
+install.packages(c(
+# Database management
+"RODBC",
+"ckanr",
+
+# Spatial analysis
+"rgdal",
+"sp",
+"maptools",
+"vegan",
+
+# Data management
+"Hmisc",
+"httr",
+"plyr",
+"dplyr",
+"lubridate",
+"stringr",
+"tidyjson",
+"tidyr",
+
+# Visualisation
+"DT",
+"mapview",
+"leaflet",
+"ggplot2"
+))
