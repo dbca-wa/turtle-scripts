e149ff2fe5c24743d80a3817c2ab1d882fc96b93
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-03-03 08:41:27 +0800

    refactor package loading

---
+++ config/load.R
@@ -0,0 +1,26 @@
+library(ckanr)
+
+# Spatial analysis
+library(rgdal)
+library(sp)
+library(maptools)
+library(vegan)
+library(PCNM)
+library(adespatial)
+
+# Data management
+library(Hmisc)
+library(htmlTable)
+library(httr)
+library(plyr)
+library(dplyr) # load after PCNM to be default for "select"
+library(lubridate)
+library(stringr)
+library(tidyjson)
+library(tidyr)
+
+# Visualisation
+library(DT)
+library(mapview)
+library(leaflet)
+library(ggplot2)
