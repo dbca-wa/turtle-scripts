354ec0490ac6477bd4295c315801dfecd7c3b986
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-04-21 10:01:16 +0800

    req app latest

--- requirements/BiosysTurtlesRequirements/app.R
+++ requirements/BiosysTurtlesRequirements/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(gh)
 library(tidyverse)
 library(magrittr)
+library(markdown)
 library(DT)
 # TODO set Github access token
 
@@ -9,18 +10,38 @@ map_chr_hack <- function(.x, .f, ...) {
   map(.x, .f, ...) %>% map_if(is.null, ~ NA_character_) %>% flatten_chr()
 }
 
+as_url <- function(url, label) {
+  paste0("<a href=\"", url, "\" target=\"_\">", label, "</a>")
+}
+
+md_to_html <- function(md) {
+  if (is.null(md)) return("")
+  markdown::markdownToHTML(file = NULL, text = md, fragment.only = TRUE)
+}
+
 gh_issues_url <- "/repos/parksandwildlife/biosys-turtles/issues"
 gh_projects_url <- "/repos/parksandwildlife/biosys-turtles/projects"
 gh_milestones_url <- "/repos/parksandwildlife/biosys-turtles/milestones"
 
-ui <- navbarPage("Biosys Turtles Requirements",
+ui <- navbarPage(
+  "Biosys Turtles Requirements",
   tabPanel("Business Needs", dataTableOutput("business_needs")),
-  tabPanel("Requirements", dataTableOutput("requirements"))
+  tabPanel("Requirements",
+           dataTableOutput("requirements"),
+           uiOutput("issue_selector")#,
+           #uiOutput("issue_detail")
+           )
 )
 
 server <- function(input, output) {
-  issue_list <- reactive(gh(gh_issues_url, state = "all", .limit = Inf))
-  milestone_list <- reactive(gh(gh_milestones_url, state = "all", .limit = Inf))
+
+  issue_list <- reactive(
+    withProgress(message = 'Loading requirements...',
+                 {gh(gh_issues_url, state = "all", .limit = Inf)}))
+
+  milestone_list <- reactive(
+    withProgress(message = 'Loading business needs...',
+                 {gh(gh_milestones_url, state = "all", .limit = Inf)}))
 
   issues <- reactive(
     issue_list() %>% {
@@ -28,11 +49,11 @@ server <- function(input, output) {
       id = map_int(., "id"),
       number = map_int(., "number"),
       title = map_chr(., "title"),
-      body = map_chr_hack(., "body"),
+      body = map_chr_hack(., "body"), # %>% map(md_to_html),
       state = map_chr(., "state"),
-      html_url = map_chr(., "html_url"),
-      comments_url = map_chr(., "comments_url"),
-      labels_url = map_chr(., "labels_url"),
+      html_url = map_chr(., "html_url") %>% map(as_url, "View"),
+      comments_url = map_chr(., "comments_url") %>% map(as_url, "View"),
+      labels_url = map_chr(., "labels_url") %>% map(as_url, "View"),
       created_by = map_chr(., c("user", "login")),
       assignee = map_chr(., c("assignee", "login")),
       milestone = map_chr(., c("milestone", "title")),
@@ -78,13 +99,27 @@ server <- function(input, output) {
         Date = due_at,
         Title = title,
         KanboardItem = html_url
+        # Requirement = body
       )
   )
 
-  output$business_needs <- renderDataTable(business_needs())
-  output$requirements <- renderDataTable(requirements())
+  output$business_needs <- shiny::renderDataTable(business_needs(), escape = FALSE)
+
+  output$requirements <- shiny::renderDataTable(requirements(), escape = FALSE)
+
+  output$issue_selector <- renderUI(
+    selectizeInput("selected_issue",
+    "Show details for requirement",
+    setNames(rownames(issues()), issues()$title),
+    selected = NULL, multiple = FALSE, options = NULL))
+
+  issue_body <- reactive({
+    if (is.na(issues())) return("<p>Loading...</p>")
+    issues()[input$selected_issue,]$body
+    })
+
+  #output$issue_detail <- reactive(renderMarkdown(file=NULL, text=issue_body()))
 
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
