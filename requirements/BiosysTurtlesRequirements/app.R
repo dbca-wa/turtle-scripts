52dc0c5365b3fa23be991873a8501b29883ae453
Florian Mayer <Florian.Mayer@dpaw.wa.govst login: Thu Sep 15 12:05:04 2016 from aws-guacamole-002.corporateict.domain.au>
2017-04-24 15:39:46 +0800

    clean up req app

--- requirements/BiosysTurtlesRequirements/app.R
+++ requirements/BiosysTurtlesRequirements/app.R
@@ -6,23 +6,40 @@ library(stringr)
 library(markdown)
 library(DT)
 library(networkD3)
-# TODO set Github access token
 
+#------------------------------------------------------------------------------#
+# Settings - change as appropriate
+#
+Sys.setenv(GH_OWNER = "parksandwildlife")
+Sys.setenv(GH_REPO = "biosys-turtles")
+
+#------------------------------------------------------------------------------#
+# Helper functions
+#
+#' Map given function, handle null as NA and flatten_chr()
 map_chr_hack <- function(.x, .f, ...) {
   map(.x, .f, ...) %>% map_if(is.null, ~ NA_character_) %>% flatten_chr()
 }
 
+# Format url and label as HTML hyperlink
 as_url <- function(url, label) {
   paste0("<a href=\"", url, "\" target=\"_\">", label, "</a>")
 }
 
+# Convert a string of markdown to HTML
 md_to_html <- function(md) {
   if (is.null(md) || is.na(md)) {md <- "no content"}
   markdown::markdownToHTML(file = NULL, text = md, fragment.only = TRUE)
 }
 
+# Extract a element with name or number n from a list x
 extract_name <- function(x, n)sapply(x, `[[`, n)
 
+# Extract a list of mentioned GitHub issue numbers from text
+# require(testthat)
+# testthat::expect_equal(extract_related("#5 #65 #123 #7# #test # test ## #8\n#9"), c(5, 65, 123, 8, 9))
+# testthat::expect_equal(extract_related("#7# #test # test ## "), NA_integer_)
+# testthat::expect_equal(extract_related(""), NA_integer_)
 extract_related <- function (x) {
   out <- x %>%
     str_extract_all("(?<=#)\\d+(?=\\s|\\n|\\<|$)") %>%
@@ -34,115 +51,128 @@ extract_related <- function (x) {
   if (length(out) == 0) return(NA_integer_) else return(out)
 }
 
-gh_issues_url <- "/repos/parksandwildlife/biosys-turtles/issues"
-gh_projects_url <- "/repos/parksandwildlife/biosys-turtles/projects"
-gh_milestones_url <- "/repos/parksandwildlife/biosys-turtles/milestones"
-gh_labels_url <- "/repos/parksandwildlife/biosys-turtles/labels"
 
+#------------------------------------------------------------------------------#
+# UI
+#
 ui <- navbarPage(
   "Biosys Turtles Requirements",
-      tabPanel(
-        "Explore",
-         column(
-           7,
-           forceNetworkOutput("force")),
-         column(
-           5,
-           uiOutput("issue_selector"),
-           uiOutput("issue_detail"))),
-      tabPanel("Business Needs", dataTableOutput("business_needs")),
-      tabPanel("Requirement List",dataTableOutput("requirements"))
+
+  tabPanel(
+    "Explore",
+
+    column(
+      7,
+      forceNetworkOutput("force")),
+
+    column(
+      5,
+      uiOutput("issue_selector"),
+      uiOutput("issue_detail"))),
+
+  tabPanel(
+    "Business Needs",
+    dataTableOutput("business_needs")),
+
+  tabPanel(
+    "Requirement List",
+    # uiOutput("category_selector"),
+    # uiOutput("priority_selector"),
+    dataTableOutput("requirements"))
 )
 
+#------------------------------------------------------------------------------#
+# Server
+#
 server <- function(input, output) {
 
+  #----------------------------------------------------------------------------#
+  # Load data from GitHub API
+  #
   issue_list <- reactive(
     withProgress(message = 'Loading requirements...',
-                 {gh(gh_issues_url, state = "all", .limit = Inf)}))
+                 {gh("/repos/:owner/:repo/issues",
+                     owner = Sys.getenv("GH_OWNER"),
+                     repo = Sys.getenv("GH_REPO"),
+                     state = "all", .limit = Inf)}))
 
   milestone_list <- reactive(
     withProgress(message = 'Loading business needs...',
-                 {gh(gh_milestones_url, state = "all", .limit = Inf)}))
+                 {gh("/repos/:owner/:repo/milestones",
+                     owner = Sys.getenv("GH_OWNER"),
+                     repo = Sys.getenv("GH_REPO"),
+                     state = "all", .limit = Inf)}))
 
   labels_list <- reactive(
     withProgress(message = 'Loading requirement categories...',
-                 {gh(gh_labels_url, state = "all", .limit = Inf)}))
+                 {gh("/repos/:owner/:repo/labels",
+                     owner = Sys.getenv("GH_OWNER"),
+                     repo = Sys.getenv("GH_REPO"),
+                     state = "all", .limit = Inf)}))
 
-  issues <- reactive(
+  #----------------------------------------------------------------------------#
+  # Transform primary data
+  #
+  issues <- reactive({
     issue_list() %>% {
-    tibble::tibble(
-      id = map_int(., "number"),
-      title = map_chr(., "title"),
-      body = map_chr_hack(., "body") %>% map(md_to_html),
-      related = body %>% map(extract_related),
-      state = map_chr(., "state"),
-      html_url = map_chr(., "html_url") %>% map(as_url, "View issue"),
-      comments_url = map_chr(., "comments_url") %>% map(as_url, "View comments"),
-      labels_url = map_chr(., "labels_url") %>% map(as_url, "View labels"),
-      labels = map(., "labels") %>% map(extract_name, "name"),
-      created_by = map_chr(., c("user", "login")),
-      assignee = map_chr(., c("assignee", "login")),
-      milestone = map_chr(., c("milestone", "title")),
-      created_at = map_chr(., "created_at") %>% as.Date(),
-      updated_at = map_chr_hack(., "updated_at") %>% as.Date(),
-      due_at = map_chr_hack(., "due_on") %>% as.Date(),
-      closed_at = map_chr_hack(., "closed_at") %>% as.Date()
-    )} %>% arrange(id)
-  )
-
-  milestones <- reactive(
-    milestone_list() %>% {
-    tibble::tibble(
-      id = map_int(., "id"),
-      number = map_int(., "number"),
-      title = map_chr(., "title"),
-      description = map_chr_hack(., "description"),
-      state = map_chr(., "state"),
-      html_url = map_chr(., "html_url"),
-      labels_url = map_chr(., "labels_url"),
-      created_by = map_chr(., c("creator", "login")),
-      created_at = map_chr(., "created_at") %>% as.Date(),
-      updated_at = map_chr_hack(., "updated_at") %>% as.Date(),
-      due_at = map_chr_hack(., "due_on") %>% as.Date(),
-      closed_at = map_chr_hack(., "closed_at") %>% as.Date()
-    )
-  })
-
-
-  labels <- reactive(
-    labels_list() %>% {
-    tibble::tibble(
-      id = map_int(., "id"),
-      url = map_chr(., "url") %>% map(as_url, "View requirements"),
-      name = map_chr(., "name"),
-      colour = map_chr(., "color")
-    )
-  })
-
-  business_needs <- reactive(
-    milestones() %>%
-      transmute(
-        ID = id,
-        Date = due_at,
-        Source = created_by,
-        Requirements = title
+      tibble::tibble(
+        id = map_int(., "number"),
+        title = map_chr(., "title"),
+        body = map_chr_hack(., "body") %>% map(md_to_html),
+        related = body %>% map(extract_related),
+        state = map_chr(., "state"),
+        html_url = map_chr(., "html_url") %>% map(as_url, "View issue"),
+        comments_url = map_chr(., "comments_url") %>% map(as_url, "View comments"),
+        labels_url = map_chr(., "labels_url") %>% map(as_url, "View labels"),
+        labels = map(., "labels") %>% map(extract_name, "name"),
+        created_by = map_chr(., c("user", "login")),
+        assignee = map_chr(., c("assignee", "login")),
+        milestone = map_chr(., c("milestone", "title")),
+        created_at = map_chr(., "created_at") %>% as.Date(),
+        updated_at = map_chr_hack(., "updated_at") %>% as.Date(),
+        due_at = map_chr_hack(., "due_on") %>% as.Date(),
+        closed_at = map_chr_hack(., "closed_at") %>% as.Date()
       )
-  )
-
-  output$label_selector <- renderUI({
-    d <- labels()
-    if (is.null(d)) return(NULL)
-    selectizeInput(
-      "selected_labels",
-      "Show categories",
-      setNames(d$name, d$name),
-      multiple = TRUE)
+    } %>% arrange(id)
   })
 
+  milestones <- reactive({
+    milestone_list() %>% {
+      tibble::tibble(
+        id = map_int(., "id"),
+        number = map_int(., "number"),
+        title = map_chr(., "title"),
+        description = map_chr_hack(., "description"),
+        state = map_chr(., "state"),
+        html_url = map_chr(., "html_url"),
+        labels_url = map_chr(., "labels_url"),
+        created_by = map_chr(., c("creator", "login")),
+        created_at = map_chr(., "created_at") %>% as.Date(),
+        updated_at = map_chr_hack(., "updated_at") %>% as.Date(),
+        due_at = map_chr_hack(., "due_on") %>% as.Date(),
+        closed_at = map_chr_hack(., "closed_at") %>% as.Date()
+      )
+    }
+  })
+
+  labels <- reactive({
+    labels_list() %>% {
+      tibble::tibble(
+        id = map_int(., "id"),
+        url = map_chr(., "url") %>% map(as_url, "View requirements"),
+        name = map_chr(., "name"),
+        colour = map_chr(., "color")
+      )
+    }
+  })
+
+  #----------------------------------------------------------------------------#
+  # Prepare secondary data
+  #
   requirements <- reactive({
     d <- issues()
     if (is.null(d)) return(NULL)
-    d %>%
+    out <- d %>%
       transmute(
         ID = id,
         Source = html_url,
@@ -150,10 +180,67 @@ server <- function(input, output) {
         Related = related,
         Title = title,
         Categories = labels
-        # Requirement = body
-      ) #%>% filter(d, Categories %in% input$selected_labels)
+        #, Requirement = body # too large to include
+      )
+    # TODO: For each element in selected_*, include rows
+    out
   })
 
+  business_needs <- reactive({
+    d <- milestones()
+    if (is.null(d)) return(NULL)
+    d %>%
+      transmute(
+        ID = number,
+        # Date = due_at,
+        # Source = created_by,
+        Requirements = title
+      )
+  })
+
+  #----------------------------------------------------------------------------#
+  # Build dropdowns
+  #
+  output$issue_selector <- renderUI({
+    d <- issues()
+    if (is.null(d)) return(NULL)
+    selectizeInput(
+      "selected_issue",
+      "Show details for requirement",
+      setNames(rownames(d), d$title),
+      width = '100%')
+  })
+
+  output$category_selector <- renderUI({
+    d <- labels()
+    if (is.null(d)) return(NULL)
+    vals <- d %>% filter(grepl("Req", name)) %>% select(name) %>% extract2(1)
+    selectizeInput(
+      "selected_categories",
+      "Categories",
+      vals,
+      width = '50%',
+      selected = vals,
+      multiple = TRUE)
+  })
+
+  output$priority_selector <- renderUI({
+    d <- labels()
+    if (is.null(d)) return(NULL)
+    vals <- d %>% filter(grepl("have", name)) %>% select(name) %>% extract2(1)
+    # vals <- c("Must have", "Should have", "Could have", "Won't have")
+    selectizeInput(
+      "selected_priorities",
+      "Priorities",
+      vals,
+      width = '50%',
+      selected = vals,
+      multiple = TRUE)
+  })
+
+  #----------------------------------------------------------------------------#
+  # Build datatables
+  #
   output$business_needs <- shiny::renderDataTable(
     business_needs(),
     options = list(filter = 'top'),
@@ -166,25 +253,12 @@ server <- function(input, output) {
     escape = FALSE
   )
 
-  output$issue_selector <- renderUI({
-    d <- issues()
-    if (is.null(d)) return(NULL)
-    selectizeInput(
-      "selected_issue",
-      "Show details for requirement",
-      setNames(rownames(d), d$title),
-      width = '100%')
-  })
-
   output$issue_detail <- renderUI({
     d <- issues()
     sel <- input$selected_issue
     if (is.null(d) || is.null(sel)) return(NULL)
     HTML(d$body[[as.integer(sel)]])
   })
-
-
-  library(networkD3)
 
   relations <- reactive({
     d <- issues()
