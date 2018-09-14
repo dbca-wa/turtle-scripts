13f80582c5e385e8fc69cfe30ddbebddf1c14881
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2018-09-14 13:39:12 +0800

    hexsticker; some cleanup of turtle program code

---
+++ testing/hexsticker.R
@@ -0,0 +1,30 @@
+hexSticker::sticker("data/hatchling.png",
+                    s_x=1,
+                    s_y=0.8,
+                    s_width=0.75,
+                    package="wastdr",
+                    p_color="white",
+                    p_size=30,
+                    p_x=1,
+                    p_y=1.45,
+                    p_family="wqy-microhei",
+                    h_fill="red",
+                    h_color="black",
+                    filename="data/wastdr.png")
+
+
+hexSticker::sticker("data/dbca.jpg",
+                    s_x=1,
+                    s_y=0.75,
+                    s_width=0.85,
+                    package="dbcR",
+                    p_color="black",
+                    p_size=40,
+                    p_x=1,
+                    p_y=1.3,
+                    p_family="Amiri",
+                    h_fill="white",
+                    h_color="black",
+                    filename="data/dbcr.png")
+
+
