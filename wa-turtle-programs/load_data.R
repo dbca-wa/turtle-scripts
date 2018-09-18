c79c06f317e3beb00c24b49c90bb737d850e74b5
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2018-09-18 18:07:49 +0800

    load data: include survey edit link; update WPTP book

--- wa-turtle-programs/load_data.R
+++ wa-turtle-programs/load_data.R
@@ -2,6 +2,7 @@ if (file.exists(here::here("data", "tracks.Rda"))) {
   load(here::here("data", "tracks.Rda"))
 } else {
   library(magrittr)
+  wastd_url <- get_wastd_url()
   q <- list(taxon = "Cheloniidae", format = "json")
   animal_records <- wastdr::wastd_GET("animal-encounters", query = q)
   animals <- wastdr::parse_animal_encounters(animal_records)
@@ -9,7 +10,9 @@ if (file.exists(here::here("data", "tracks.Rda"))) {
   tracks_all <- wastdr::parse_turtle_nest_encounters(track_records)
   disturbance <- wastdr::wastd_GET("disturbance-observations") %>%
     wastdr::parse_disturbance_observations()
-  surveys <- wastd_GET("surveys") %>% wastdr::parse_surveys()
+  surveys <- wastd_GET("surveys") %>% wastdr::parse_surveys() %>%
+    dplyr::mutate(change_url = glue::glue(
+      '<a href="{wastd_url}/admin/observations/survey/{id}/change/" target="_">Update Survey {id}</a>'))
   save(
     animal_records,
     animals,
