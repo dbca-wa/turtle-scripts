5ab9f9ba3eadef300b61883e465120434cc7b264
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2018-10-16 21:43:33 +0800

    load_data

--- wa-turtle-programs/load_data.R
+++ wa-turtle-programs/load_data.R
@@ -2,7 +2,7 @@ if (file.exists(here::here("data", "tracks.Rda"))) {
   load(here::here("data", "tracks.Rda"))
 } else {
   library(magrittr)
-  wastd_url <- get_wastd_url()
+  wastd_url <- wastdr::get_wastd_url()
   q <- list(taxon = "Cheloniidae", format = "json")
   animal_records <- wastdr::wastd_GET("animal-encounters", query = q)
   animals <- wastdr::parse_animal_encounters(animal_records)
@@ -10,7 +10,7 @@ if (file.exists(here::here("data", "tracks.Rda"))) {
   tracks_all <- wastdr::parse_turtle_nest_encounters(track_records)
   disturbance_records <- wastdr::wastd_GET("disturbance-observations")
   disturbance <- disturbance_records %>% wastdr::parse_disturbance_observations()
-  survey_records <- wastd_GET("surveys")
+  survey_records <- wastdr::wastd_GET("surveys")
   surveys <- survey_records %>%
     wastdr::parse_surveys() %>%
     dplyr::mutate(change_url = glue::glue(
