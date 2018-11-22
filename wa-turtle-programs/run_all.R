e9e76623f3a9366f41e851cb44fa6d4f3078404c
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2018-11-22 18:26:54 +0800

    update run_all

--- wa-turtle-programs/run_all.R
+++ wa-turtle-programs/run_all.R
@@ -36,16 +36,16 @@ save(
 
 # Analysis - upload html output after render
 rmarkdown::render(here::here("wa-turtle-programs", "QA.Rmd"))
-ckanr::resource_update("be63cdd8-4aba-4329-b333-8090b076c792", "QA.html")
+rmarkdown::render(here::here("wa-turtle-programs", "QA.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "lgcs.Rmd"))
-ckanr::resource_update("49b31e39-7b40-45cb-aa6f-dabb4c692b69", here::here("wa-turtle-programs", "lgcs.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "lgcs.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "broome.Rmd"))
-ckanr::resource_update("9572e76b-55e9-4afb-b224-7cce7beb4959", here::here("wa-turtle-programs", "broome.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "broome.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "emb.Rmd"))
-ckanr::resource_update("56f1071d-b8ed-4ac7-bb37-a5151978a9f1", here::here("wa-turtle-programs", "emb.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "emb.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "pthedland.Rmd"))
 rmarkdown::render(here::here("wa-turtle-programs", "pthedland.Rmd"))
@@ -54,12 +54,12 @@ rmarkdown::render(here::here("wa-turtle-programs", "westpilbara.Rmd"))
 rmarkdown::render(here::here("wa-turtle-programs", "westpilbara.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "rosemary.Rmd"))
-ckanr::resource_update("aa7a5b85-4606-4e18-bdd4-407d6a887db1", here::here("wa-turtle-programs", "rosemary.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "rosemary.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "delambre.Rmd"))
-ckanr::resource_update("69d5585a-3307-41ee-980e-f829f7eef004", here::here("wa-turtle-programs", "delambre.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "delambre.Rmd"))
 
 rmarkdown::render(here::here("wa-turtle-programs", "thevenard.Rmd"))
-ckanr::resource_update("19294b29-a70f-45e3-b06b-fc1eefe858d2", here::here("wa-turtle-programs", "thevenard.html"))
+rmarkdown::render(here::here("wa-turtle-programs", "thevenard.Rmd"))
 
 # rmarkdown::render(here::here("wa-turtle-programs", "ningaloo.Rmd"))
