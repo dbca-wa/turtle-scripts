148e0a8427a1117df0749a4d5b070f4b499c91d1
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2019-02-22 17:21:59 +0800

    CFH tagged nests updated

--- wa-turtle-programs/tracks_helpers.R
+++ wa-turtle-programs/tracks_helpers.R
@@ -310,8 +310,9 @@ filter_missing_site <- . %>% dplyr::filter(is.na(site_id))
 # Surveys
 exclude_training_surveys <- . %>% dplyr::filter(is_production == TRUE)
 filter_surveys_requiring_qa <- . %>%
-  dplyr::filter(grepl("QA", start_comments) | grepl("QA", end_comments)) %>%
-  dplyr::select(change_url, turtle_date, site_name, reporter, reporter_username, start_comments, end_comments)
+  dplyr::filter(grepl("NEEDS QA", start_comments) | grepl("NEEDS QA", end_comments)) %>%
+  dplyr::select(change_url, turtle_date, site_name, reporter, reporter_username,
+                start_comments, end_comments)
 
 filter_surveys_missing_end <- . %>%
   dplyr::filter(is.na(end_source_id)) %>%
@@ -346,10 +347,10 @@ map_tracks <- function(
 
   . <- NULL
   layersControlOptions <- NULL
-  if (cluster==TRUE){
+  if (cluster == TRUE){
     co = markerClusterOptions()
   } else {
-    co=NULL
+    co = NULL
   }
 
   l <- leaflet(width = 800, height = 600) %>%
@@ -376,9 +377,8 @@ map_tracks <- function(
           "<p>Seen on {format(datetime, fmt)} AWST by {observer}",
           "<p>Survey {survey_id} at {site_name} ",
           "{format(survey_start_time, fmt)}-{format(survey_end_time, fmt)} AWST</p>",
-          "<p><a class=\"btn btn-xs btn-primary\" target=\"_\" rel=\"nofollow\" ",
-          "href=\"{wastd_url}{absolute_admin_url}\">",
-          "<span class=\"text-white\">Edit on WAStD</span></a></p>"),
+          "<p><a class=\"btn btn-xs btn-secondary\" target=\"_\" rel=\"nofollow\" ",
+          "href=\"{wastd_url}{absolute_admin_url}\">Edit on WAStD</a></p>"),
         group = df,
         clusterOptions = co)
     })
@@ -388,4 +388,45 @@ map_tracks <- function(
     overlayGroups = names(tracks.df),
     options = layersControlOptions(collapsed = FALSE)
   )
+}
+
+map_nests <- function (data,
+                       wastd_url=wastdr::get_wastd_url(),
+                       fmt = "%d/%m/%Y %H:%M",
+                       cluster=FALSE)
+{
+  . <- NULL
+  layersControlOptions <- NULL
+  if (cluster == TRUE){
+    co = markerClusterOptions()
+  } else {
+    co = NULL
+  }
+  pal <- leaflet::colorFactor(palette = "RdYlBu", domain = data$tag_status)
+  l <- leaflet::leaflet(width = 800, height = 600) %>%
+    leaflet::addProviderTiles("Esri.WorldImagery", group = "Aerial") %>%
+    leaflet::addProviderTiles("OpenStreetMap.Mapnik", group = "Place names") %>%
+    leaflet::clearBounds() %>%
+    leaflet::addAwesomeMarkers(
+      data = data, lng = ~longitude,
+      lat = ~latitude,
+      icon = leaflet::makeAwesomeIcon(
+        icon = "tag", text = ~tag_label, markerColor = ~pal(tag_status)),
+      label = ~glue::glue(
+        "{format(datetime, \"%d/%m/%Y %H:%M\")} {tag_status} ",
+        "{flipper_tag_id} {date_nest_laid} {tag_label}"),
+      popup = ~glue::glue(
+        "<h3>{flipper_tag_id} {date_nest_laid} {tag_label}</h3>",
+        "<p>{humanize(tag_status)} on {format(datetime, fmt)} AWST by {observer}",
+        "<p>Survey {survey_id} at {site_name} ",
+        "{format(survey_start_time, fmt)}-{format(survey_end_time, fmt)} AWST</p>",
+        "<p><a class=\"btn btn-xs btn-secondary\" target=\"_\" rel=\"nofollow\" ",
+        "href=\"{wastd_url}{absolute_admin_url}\">Edit on WAStD</a></p>"),
+      group = "Nests",
+      clusterOptions = co) %>%
+    leaflet::addLayersControl(
+      baseGroups = c("Aerial", "Place names"),
+      overlayGroups = c("Nests"),
+      options = leaflet::layersControlOptions(collapsed = FALSE))
+  l
 }
